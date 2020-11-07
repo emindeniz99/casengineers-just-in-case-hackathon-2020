0bc0af918906f2e4f9d2a335c7aef77b4adc2b5b
umutsolmaz <48951476+bouncer535@users.noreply.github.com>
2020-11-07 23:29:42 +0300

    v

--- src/pages/Interview.jsx
+++ src/pages/Interview.jsx
@@ -10,9 +10,31 @@ const Interview = () => {
 
 
 	return <div>
+		<h1>Deniz Umut Şengün</h1>
+		<br/>
+		<br/>
+		<h1>Kendinizden bahseder misiniz ?</h1>
 		<ReactPlayer
             url="https://www.youtube.com/watch?v=_oWyEWvoq4k"
         />
+		<br/>
+		<br/>
+		<h1>Güçlü  ve Zayıf yönleriniz nelerdir ?</h1>
+		<ReactPlayer
+            url="https://www.youtube.com/watch?v=QyUlSH2lkNY"
+        />
+		<br/>
+		<br/>
+		<h1>Maaş beklentiniz nedir ?</h1>
+		<ReactPlayer
+            url="https://www.youtube.com/watch?v=rxcy94gsPmQ"
+        />
+		<br/>
+		<br/>
+		<h1>karşılaştığınız zor bir durumu ve onu nasıl çözdüğünüzü anlatabilir misiniz ?</h1>
+		<ReactPlayer
+            url="https://www.youtube.com/watch?v=hnl1MSLD4ys"
+        />		
 	</div>
 }
 
