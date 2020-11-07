75579cea519f19dd567f238f47cd6d084d02b009
umutsolmaz <48951476+bouncer535@users.noreply.github.com>
2020-11-07 13:59:27 +0300

    timeline

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,7 +1,62 @@
-import React from "react"
+import { Button, ButtonGroup, Form, Card, CardDeck,Row, ListGroup } from "react-bootstrap"
+import React, { useEffect, useState } from "react"
+import { firestore, storage } from "../firebase"
+
+
+
 
 const Timeline = () => {
-	return <div>Timeline Page</div>
+
+	const [profileCards, setProfileCards] = useState([])
+	useEffect(() => {
+		firestore
+			.collection("users")
+			.get()
+			.then((userCol) => {
+				let usc = userCol.docs.map((userDoc) => {
+					let data = userDoc.data()
+					return {
+						id: userDoc.id,
+						cvUrl: data.cvUrl,
+					}
+				})
+				setProfileCards(usc)
+			})
+	}, [])
+
+
+	return <div>
+		<CardDeck   style={{ width: '1000px'}} >
+			{profileCards.map((i) => (
+				 <Row>
+					<Card style={{ width: '450px', height: '1000px', marginBottom: '50px', marginRight: '50px' }}>
+					<Card.Img variant="top" src={i.cvUrl} style={{ height: '750px'}} />
+					<Card.Body>
+						<Card.Title>{i.id}</Card.Title>
+						<ListGroup variant="flush">
+							<ListGroup.Item>
+									 <Card.Text>
+										Cv hakkında yorum giriniz
+									</Card.Text>
+							</ListGroup.Item>
+
+							<ListGroup.Item>	
+								<input  style={{ width: '350px'}} />
+							</ListGroup.Item>
+
+							<ListGroup.Item>
+								<Button variant="primary">Yorumu Gönder</Button>
+								</ListGroup.Item>
+						</ListGroup>
+					
+					
+						
+					</Card.Body>
+					</Card>
+				</Row>
+				))}
+		</CardDeck>
+	</div>
 }
 
 export default Timeline
