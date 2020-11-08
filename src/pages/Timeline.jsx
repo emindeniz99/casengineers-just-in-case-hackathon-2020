9e31bc4cd42dfd868977330c4c48df9cdc135aa8
EminDeniz99 <emindeniz99@gmail.com>
2020-11-08 04:41:45 +0300

    add comment functionality

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -1,12 +1,16 @@
-import { Button, ButtonGroup, Form, Card, CardDeck,Row, ListGroup } from "react-bootstrap"
+import {
+	Button,
+	ButtonGroup,
+	Card,
+	CardDeck,
+	Form,
+	ListGroup,
+	Row,
+} from "react-bootstrap"
 import React, { useEffect, useState } from "react"
 import { firestore, storage } from "../firebase"
 
-
-
-
 const Timeline = () => {
-
 	const [profileCards, setProfileCards] = useState([])
 	useEffect(() => {
 		firestore
@@ -24,39 +28,70 @@ const Timeline = () => {
 			})
 	}, [])
 
-
-	return <div>
-		<CardDeck   style={{ width: '1000px'}} >
-			{profileCards.map((i) => (
-				 <Row>
-					<Card style={{ width: '450px', height: '1000px', marginBottom: '50px', marginRight: '50px' }}>
-					<Card.Img variant="top" src={i.cvUrl} style={{ height: '750px'}} />
-					<Card.Body>
-						<Card.Title>{i.id}</Card.Title>
-						<ListGroup variant="flush">
-							<ListGroup.Item>
-									 <Card.Text>
-										Cv hakkında yorum giriniz
-									</Card.Text>
-							</ListGroup.Item>
-
-							<ListGroup.Item>	
-								<input  style={{ width: '350px'}} />
-							</ListGroup.Item>
-
-							<ListGroup.Item>
-								<Button variant="primary">Yorumu Gönder</Button>
-								</ListGroup.Item>
-						</ListGroup>
-					
-					
-						
-					</Card.Body>
-					</Card>
-				</Row>
+	return (
+		<div>
+			<CardDeck style={{ width: "1000px" }}>
+				{profileCards.map((i, index) => (
+					<CVCard i={i} key={index} />
 				))}
-		</CardDeck>
-	</div>
+			</CardDeck>
+		</div>
+	)
 }
 
 export default Timeline
+
+const CVCard = ({ i }) => {
+	const [comment, setcomment] = useState("")
+
+	return (
+		<Row>
+			<Card
+				style={{
+					width: "450px",
+					height: "1000px",
+					marginBottom: "50px",
+					marginRight: "50px",
+				}}
+			>
+				<Card.Img variant="top" src={i.cvUrl} style={{ height: "750px" }} />
+				<Card.Body>
+					<Card.Title>{i.id}</Card.Title>
+					<ListGroup variant="flush">
+						<ListGroup.Item>
+							<Card.Text>Cv hakkında yorum giriniz</Card.Text>
+						</ListGroup.Item>
+
+						<ListGroup.Item>
+							<input
+								value={comment}
+								style={{ width: "350px" }}
+								onChange={(e) => {
+									setcomment(e.target.value)
+								}}
+							/>
+						</ListGroup.Item>
+
+						<ListGroup.Item>
+							<Button
+								variant="primary"
+								onClick={() => {
+									firestore.collection("comments").add({
+										content: comment,
+										from:
+											window.localStorage.getItem("name") ??
+											"Demo Kullanıcı " + Math.floor(Math.random() * 10),
+										point: 7,
+										time: "Kasım 2020",
+									})
+								}}
+							>
+								Yorumu Gönder
+							</Button>
+						</ListGroup.Item>
+					</ListGroup>
+				</Card.Body>
+			</Card>
+		</Row>
+	)
+}
